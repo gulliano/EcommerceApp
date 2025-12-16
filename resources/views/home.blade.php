35fe289e8db4cbd660618567d18284371a30a7f0
gulliano <ggulliano@msn.com>
2025-12-16 11:06:50 -0400

    exercice nn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 @section('content')
  
 
+
+
 <div class="bg-white">
   <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
     <h2 class="text-2xl font-bold tracking-tight text-gray-900">Les produits vedettes</h2>
@@ -12,6 +14,12 @@
         @foreach ($featuredProducts as $featuredProduct )
               <x-card-product :product="$featuredProduct" />
         @endforeach
+
+
+
+
+
+
       
     
      
--- resources/views/layouts/boutique.blade.php
+++ resources/views/layouts/boutique.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -28,5 +30,8 @@
 
     <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
     <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
+
+
     </body>
 </html>
